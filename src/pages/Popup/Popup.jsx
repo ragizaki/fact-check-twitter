da83e043135f52c4c553ea02ba9195d04bd3a54c
Sanju311 <sanjumoorthy622@gmail.com>
2023-03-18 17:47:47 -0400

    Created extension dropdown, need to add svg file of logo, round borders and fix slider

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { Component } from 'react';
+import logo from '../../assets/img/logo_real.png';
+//import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Switch } from '@chakra-ui/react'
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
-};
+class Popup extends Component {
+
+  state = {}
+
+  render() {
+
+    return (
+      <ChakraProvider>
+        <div className="App">
+          <header className="d-flex flex-column app-header">
+            <div className="text-center intro">Fact Check the Internet With</div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <hr />
+            <p className="main-text">
+              Insight is a real fact checking tool powered by AI to do stuff. Runs in the background
+            and can validate tweets from politicians. <a href="google.com" target="_blank">Learn more</a>
+            </p>
+            <div className="row d-flex slider ">
+              <p className="col right">
+                Enabled
+              </p>
+              <Switch size="md" colorScheme="orange" className="col left" />
+            </div>
+          </header>
+        </div>
+      </ChakraProvider>
+
+    );
+  }
+}
+
 
 export default Popup;
+
+
+
+
+
